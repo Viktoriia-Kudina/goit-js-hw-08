8cf2093f9fab3019843603b7dc13bab123b8b105
Viktoriia Kudina <vika.kudina1994@gmail.com>
2025-11-01 17:19:39 +0200

    add all

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const textInput = document.querySelector("#name-input");
-const spanOutput = document.querySelector("#name-output");
-
-textInput.addEventListener("input", (event) => {
-  const value = event.currentTarget.value.trim();
-
-  spanOutput.textContent = value === "" ? "Anonymous" : value;
-});
